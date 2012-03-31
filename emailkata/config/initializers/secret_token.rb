4af881b4a9d1708e7216ef62c308ee9557d6498d
Jen Dockter <jendockter@gmail.com>
2012-03-31 12:14:14 -0700

    whoops

--- emailkata/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Emailkata::Application.config.secret_token = '6cfa5a8a1d46f63726f217c650cff4d0e21ecc2f3837d5ddfe5a935c8e38a9a822700f8e8e4c32f196285a96118d69c45c61a6921e489998d3839707ecbc5910'
